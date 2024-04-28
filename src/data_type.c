d570b4af57f662b40d8a971a75cb2ee7c417440a
yuxiuaoyuan0406 <973971990@qq.com>
2024-04-28 10:48:01 +0800

    add data type header
    type: array_t

---
+++ src/data_type.c
@@ -0,0 +1,27 @@
+/// @author Xiaoyuan Yu
+/// @date   2024-04-27
+
+#include "data_type.h"
+#include <stdio.h>
+
+array_t new_array(array_t_size_typedef size)
+{
+    array_t a = malloc((sizeof(array_t_size_typedef)+size*sizeof(array_t_value_typedef)));
+    // if(!a)
+    //     a = malloc((sizeof(array_t_size_typedef)+size*sizeof(array_t_value_typedef)));
+    if(!a)
+    {
+        perror("Fail to alloc new array.");
+        return NULL;
+    }
+    *(array_t_size_typedef*)&a->size = size;
+    for (array_t_size_typedef i = 0; i < size; i++)
+        a->val[i] = 0;
+    
+    return a;
+}
+
+void del_array(array_t a)
+{
+    free((void *)a);
+}
---
+++ src/data_type.h
@@ -0,0 +1,35 @@
+/// @author Xiaoyuan Yu
+/// @date   2024-04-27
+
+#ifndef __DATA_TYPE_H
+#define __DATA_TYPE_H
+
+#include <stdlib.h>
+#include <stdint.h>
+
+/// Basic data type and data object function.
+
+/// @brief size type of array data
+typedef uint32_t array_t_size_typedef;
+/// @brief value type of array data
+typedef double array_t_value_typedef;
+
+/// @brief Array data type. 
+/// 
+typedef struct 
+{
+    const array_t_size_typedef size;
+    array_t_value_typedef val[];
+}*array_t;
+
+/// @brief Create a new array object with given size.
+/// @param size size of array to create
+/// @return New array object created with given size.
+array_t new_array(array_t_size_typedef size);
+
+/// @brief Delete an array object and free the space.
+/// @param a Array to be deleted.
+void del_array(array_t a);
+
+
+#endif
