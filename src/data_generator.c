4c559a64eab08f43bd883309b366a249504b4054
yuxiuaoyuan0406 <973971990@qq.com>
2024-04-28 11:02:28 +0800

    add data_generator header
    generator for linear data and function-defined data

---
+++ src/data_generator.c
@@ -0,0 +1,38 @@
+/// @author Xiaoyuan Yu
+/// @date   2024-04-27
+
+#include "data_generator.h"
+
+array_t linear_data_generator(array_t_value_typedef begin, array_t_value_typedef step, array_t_size_typedef size)
+{
+    array_t a = new_array(size);
+    if(!a)
+        return a;
+
+    for (array_t_size_typedef i = 0; i < size; i++)
+        a->val[i] = begin+i*step;
+
+    return a;
+}
+
+void function_data_generator(array_t dest, array_t src, simple_function_t f)
+{
+    if(!IS_SAME_SIZE(dest, src))
+    {
+        fprintf(stderr, "Expecting same size array.\n");
+        return;
+    }
+
+    for (array_t_size_typedef i = 0; i < dest->size; i++)
+        dest->val[i] = f(src->val[i]);
+}
+
+array_t_value_typedef save_data(array_t a, FILE *f)
+{
+    array_t_size_typedef i;
+    for (i = 0; i < a->size; i++)
+    {
+        fprintf(f, "%.14lf\n", a->val[i]);
+    }
+    return i;
+}
---
+++ src/data_generator.h
@@ -0,0 +1,43 @@
+/// @author Xiaoyuan Yu
+/// @date   2024-04-27
+
+#ifndef __DATA_GENERATOR_H
+#define __DATA_GENERATOR_H
+
+#include "data_type.h"
+#include "common.h"
+
+/// @brief check if two array data object is same size
+#define IS_SAME_SIZE(a,b) ((a)->size == (b)->size)
+
+/// @brief type for a value-to-value math-function
+typedef array_t_value_typedef simple_function_t(array_t_value_typedef);
+
+/// @brief Linear array data generator, create a new array.
+/// @param begin The begin value of linear data.
+/// @param step The step which each value increases.
+/// @param size The size of the array.
+/// @return New linear data array object.
+array_t linear_data_generator(
+    array_t_value_typedef begin, 
+    array_t_value_typedef step,
+    array_t_size_typedef size);
+
+/// @brief Funtion defined value creator. `dest` = f(`src`)
+/// @param dest output value of the function `f`
+/// @param src input value of the function `f`
+/// @param f function which defines a value-to-value mapping
+void function_data_generator(
+    array_t dest,
+    array_t src,
+    simple_function_t f);
+
+/// @brief Save the data of a array type to text-base file.
+/// @param a Array data object.
+/// @param f File handle.
+/// @return how many lines saved
+array_t_value_typedef save_data(
+    array_t a,
+    FILE * f);
+
+#endif
