a75ffc485b8c2a525dacc8ddb983846cfbcbc0e5
yuxiuaoyuan0406 <973971990@qq.com>
2024-04-28 11:55:20 +0800

    fix: 0-length array

--- src/data_type.c
+++ src/data_type.c
@@ -6,6 +6,11 @@
 
 array_t new_array(array_t_size_typedef size)
 {
+    if (size == 0)
+    {
+        fprintf(stderr,"Cannot create a 0-length array");
+        // exit(0);
+    }
     array_t a = malloc((sizeof(array_t_size_typedef)+size*sizeof(array_t_value_typedef)));
     // if(!a)
     //     a = malloc((sizeof(array_t_size_typedef)+size*sizeof(array_t_value_typedef)));
