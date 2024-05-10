cb18e0ad4605199fdf72f41ce7d6b79a5fd86d09
yuxiuaoyuan0406 <973971990@qq.com>
2024-05-10 11:01:30 +0800

    use interpolation instead of calculate sin

--- scripts/rk.py
+++ scripts/rk.py
@@ -12,9 +12,23 @@ dumping_coef = 4.95e-6
 normal_spr_coef = spring_coef/mass
 normal_dmp_coef = dumping_coef/mass
 
+t = np.linspace(0.,2., num=2000000)
+
+# @jit(nopython=True)
+# def a(x):
+#     # return 0.00004 * np.sin(2*np.pi * 50 * t)
+#     return 40 * np.sin(2*np.pi * 50 * x)
+
+a_in = 40 * np.sin(2*np.pi * 50 * t)
+
 @jit(nopython=True)
-def a(t):
-    return 0.00004 * np.sin(2*np.pi * 50 * t)
+def a(x):
+    i = int(x*1e7)
+    if i%10 == 0:
+        return a_in[i//10]
+    else:
+        return .5*(a_in[i//10] + a_in[i//10+1])
+
 
 @jit(nopython=True)
 def f1(y,t):
@@ -47,7 +61,7 @@ y_0 = np.array([0.,0.])
 
 h = 1e-6
 
-t = [t_0]
+# t = [t_0]
 y = [y_0]
 
 _t = t_0
@@ -56,26 +70,23 @@ for i in tqdm(range(2000000-1), desc="Runge-Kutta"):
     _t, _y = runge_kutta(f1, _t, _y, h)
     # t = np.append(t, [_t], 0)
     # y = np.append(y, [_y], 0)
-    t.append(_t)
+    # t.append(_t)
     y.append(_y)
 
-t = np.array(t)
+# t = np.array(t)
 y = np.array(y)
 
 # y0 = [0.,0.]
 # t = np.linspace(0.,2., num=2000000)
 
-np.savetxt('standard_input.dat', a(t).transpose(), delimiter=' ')
+# np.savetxt('standard_input.dat', a(t).transpose(), delimiter=' ')
 np.savetxt('disp_py.dat', y, delimiter='  ')
 
 # sol = odeint(f, y0, t, args=(normal_spr_coef, normal_dmp_coef))
 
-plt.plot(t, y.transpose()[0], 'blue', label='x(t)')
-# plt.plot(t, sol[:, 0], 'blue', label='x(t)')
-# plt.plot(t, sol[:, 1], 'green', label='v(t)')
-# plt.legend(loc='best')
-plt.xlabel('t')
-plt.grid()
-plt.show()
+# plt.plot(t, y.transpose()[0], 'blue', label='x(t)')
+# plt.xlabel('t')
+# plt.grid()
+# plt.show()
 
 
